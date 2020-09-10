9ce43d3571a12d38ba13acd7931cba8b7e218929
Yaroslav Oliinyk <iamiaroslav@gmail.com>
2020-09-10 22:17:29 +0300

    Created presenter trip.js and moved an instance into main.js.

--- src/main.js
+++ src/main.js
@@ -1,34 +1,29 @@
 import TripInfoView from "./view/trip-info.js";
 import MenuView from "./view/menu.js";
 import FilterView from "./view/filter.js";
-import SortView from "./view/sort.js";
-import DayListView from "./view/day-list.js";
-import DayContainerView from "./view/day-container.js";
-import WaypointView from "./view/waypoint.js";
-import WaypointFormView from "./view/waypoint-form.js";
-import NoWaypointView from "./view/no-waypoint.js";
-import {render, RenderPosition, replace} from "./utils/render.js";
+import {render, RenderPosition} from "./utils/render.js";
 import {generateEvent} from "./mock/event.js";
 import TripPresenter from "./presenter/trip.js";
 
 const TASK_DATA_COUNT = 15;
 
-const arr = new Array(TASK_DATA_COUNT)
+const events = new Array(TASK_DATA_COUNT)
 .fill()
 .map(generateEvent)
-.sort((a, b) => {
-  return new Date(a.schedule.start) - new Date(b.schedule.start);
+.sort((date1, date2) => {
+  return new Date(date1.schedule.start) - new Date(date2.schedule.start);
 });
 
-const events = new Map();
+const eventsByDay = new Map();
 
-arr.forEach((element) => {
-  let dateWithoutTime = element.schedule.start.toDateString();
+events.forEach((event) => {
+  const date = event.schedule.start.toDateString();
 
-  if (!events.has(dateWithoutTime)) {
-    events.set(dateWithoutTime, []);
+  if (!eventsByDay.has(date)) {
+    eventsByDay.set(date, []);
   }
-  events.get(dateWithoutTime).push(element);
+
+  eventsByDay.get(date).push(event);
 });
 
 const siteMainElement = document.querySelector(`.trip-main`);
@@ -40,69 +35,6 @@ render(siteTripControlsElement.lastElementChild, new FilterView(), RenderPositio
 
 const siteEventsElement = document.querySelector(`.trip-events`);
 
-render(siteEventsElement, new SortView(), RenderPosition.BEFOREEND);
-
-render(siteEventsElement, new DayListView(), RenderPosition.BEFOREEND);
-
-const renderWaypoint = (eventsListElement, event) => {
-  const waypointComponent = new WaypointView(event);
-  const waypointFormComponent = new WaypointFormView(event);
-
-  const replaceWaypointToForm = () => {
-    replace(waypointFormComponent, waypointComponent);
-  };
-
-  const replaceFormToWaypoint = () => {
-    replace(waypointComponent, waypointFormComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === `Escape` || evt.key === `Esc`) {
-      evt.preventDefault();
-      replaceFormToWaypoint();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  waypointComponent.setEditClickHandler(() => {
-    replaceWaypointToForm();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  waypointFormComponent.setFormSubmitHandler(() => {
-    replaceFormToWaypoint();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  render(eventsListElement, waypointComponent, RenderPosition.BEFOREEND);
-};
-
-const siteDaysListElement = document.querySelector(`.trip-days`);
-
-const renderWaypointsList = (data) => {
-  let dayIndex = 1;
-
-  for (const [key, value] of data.entries()) {
-    const date = new Date(key);
-
-    render(siteDaysListElement, new DayContainerView(dayIndex, date), RenderPosition.BEFOREEND);
-
-    const siteEventsListElement = Array.from(document.querySelectorAll(`.trip-events__list`)).pop();
-
-    value.forEach((element) => {
-      renderWaypoint(siteEventsListElement, element);
-    });
-
-    dayIndex++;
-  }
-};
-
-if (events.size !== 0) {
-  renderWaypointsList(events);
-} else {
-  render(siteEventsElement, new NoWaypointView(), RenderPosition.BEFOREEND);
-}
-
 const tripPresenter = new TripPresenter(siteEventsElement);
 
-tripPresenter.init(events);
+tripPresenter.init(eventsByDay);
--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,25 +1,85 @@
-import TripInfoView from "../view/trip-info.js";
-import MenuView from "../view/menu.js";
-import FilterView from "../view/filter.js";
 import SortView from "../view/sort.js";
 import DayListView from "../view/day-list.js";
 import DayContainerView from "../view/day-container.js";
+import NoWaypointView from "../view/no-waypoint.js";
 import WaypointView from "../view/waypoint.js";
 import WaypointFormView from "../view/waypoint-form.js";
-import NoWaypointView from "../view/no-waypoint.js";
-import {render, RenderPosition} from "../utils/render.js";
-
-const TASK_DATA_COUNT = 15;
+import {render, RenderPosition, replace} from "../utils/render.js";
 
 export default class Trip {
   constructor(tripContainer) {
-    this._container = tripContainer;
-    this._renderedTaskCount = TASK_DATA_COUNT;
+    this._tripContainer = tripContainer;
 
+    this._sortComponent = new SortView();
+    this._dayListComponent = new DayListView();
+    this._noWaypointComponent = new NoWaypointView();
   }
 
   init(events) {
     this._events = new Map(events);
+
+    render(this._tripContainer, this._sortComponent, RenderPosition.BEFOREEND);
+    render(this._tripContainer, this._dayListComponent, RenderPosition.BEFOREEND);
+
+    this._renderWaypoints();
   }
 
+  _renderWaypoint(container, event) {
+    const waypointComponent = new WaypointView(event);
+    const waypointFormComponent = new WaypointFormView(event);
+
+    const replaceWaypointToForm = () => {
+      replace(waypointFormComponent, waypointComponent);
+    };
+
+    const replaceFormToWaypoint = () => {
+      replace(waypointComponent, waypointFormComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        evt.preventDefault();
+        replaceFormToWaypoint();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    waypointComponent.setEditClickHandler(() => {
+      replaceWaypointToForm();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    waypointFormComponent.setFormSubmitHandler(() => {
+      replaceFormToWaypoint();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    render(container, waypointComponent, RenderPosition.BEFOREEND);
+  }
+
+  _renderWaypointsList() {
+    let dayIndex = 1;
+
+    for (const [key, value] of this._events.entries()) {
+      const date = new Date(key);
+
+      render(this._dayListComponent, new DayContainerView(dayIndex, date), RenderPosition.BEFOREEND);
+
+      const eventsList = Array.from(document.querySelectorAll(`.trip-events__list`)).pop();
+
+      value.forEach((item) => {
+        this._renderWaypoint(eventsList, item);
+      });
+
+      dayIndex++;
+    }
+  }
+
+  _renderWaypoints() {
+    if (this._events.size !== 0) {
+      this._renderWaypointsList();
+    } else {
+      render(this._tripContainer, this._noWaypointComponent, RenderPosition.BEFOREEND);
+    }
+  }
 }
